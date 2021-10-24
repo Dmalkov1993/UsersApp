c3a60fbe54add7b13289c6a97ac7430b01475148
Мальков Денис Константинович <Malkov_DK@nposapfir.ru>
2021-10-24 11:17:06 +0500

    First commit

---
+++ PostFIOFirstService/Controllers/AcceptUserDataController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PostFIOFirstService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AcceptUserDataController : Controller
+    {
+        [HttpPost]
+        [Route("AcceptUserData")]
+        public IActionResult AcceptUserData()
+        {
+            return View();
+        }
+    }
+}
