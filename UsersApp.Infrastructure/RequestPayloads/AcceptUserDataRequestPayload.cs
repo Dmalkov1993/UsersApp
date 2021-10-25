f06f0cca5469034c9eaf4880c5b3cd7c6f748752
Мальков Денис Константинович <Malkov_DK@nposapfir.ru>
2021-10-26 01:28:46 +0500

    Немного для Swagger-а.

--- UsersApp.Infrastructure/RequestPayloads/AcceptUserDataRequestPayload.cs
+++ UsersApp.Infrastructure/RequestPayloads/AcceptUserDataRequestPayload.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
 
 namespace UsersApp.Infrastructure.RequestPayloads
 {
@@ -8,26 +9,31 @@ namespace UsersApp.Infrastructure.RequestPayloads
         /// <summary>
         /// Имя.
         /// </summary>
+        [DefaultValue("Ivan")]
         public string Name { get; set; }
 
         /// <summary>
         /// Фамилия.
         /// </summary>
+        [DefaultValue("Ivanov")]
         public string Surname { get; set; }
 
         /// <summary>
         /// Отчество.
         /// </summary>
+        [DefaultValue("Васильевич")]
         public string Patronymic { get; set; }
 
         /// <summary>
         /// Номер телефона.
         /// </summary>
+        [DefaultValue("9991113344")]
         public string PhoneNumber { get; set; }
 
         /// <summary>
         /// Email.
         /// </summary>
+        [DefaultValue("Ivanov@ya.ru")]
         public string Email { get; set; }
 
         public override string ToString()
