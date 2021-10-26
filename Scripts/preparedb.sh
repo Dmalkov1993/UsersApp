b4474e812cb7752351e70c8829ee8d9e5074894f
Мальков Денис Константинович <Malkov_DK@nposapfir.ru>
2021-10-26 12:09:16 +0500

    PrepareDbScript

--- Scripts/preparedb.sh
+++ Scripts/preparedb.sh
@@ -1,6 +1,2 @@
-psql -a -U postgres -c 'create database sso'
-psql -a -U postgres -d sso -f /docker-entrypoint-initdb.d/Scripts/InitConfigContext.sql
-psql -a -U postgres -d sso -f /docker-entrypoint-initdb.d/Scripts/InitSystemContext.sql
-psql -a -U postgres -c 'create database unit1'
-psql -a -U postgres -d unit1 -f /docker-entrypoint-initdb.d/Scripts/InitSystemContext.sql
-psql -a -U postgres -d unit1 -f /docker-entrypoint-initdb.d/Scripts/InitBusinessContext.sql
+psql -a -U postgres -c 'create database UsersApp'
+psql -a -U postgres -d usersapp -f /docker-entrypoint-initdb.d/Scripts/InitDataBase.sql
